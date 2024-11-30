b7513e9d0a09a640114dafbd19bf78e685f37efd
Aditya Singh <1211821022@ngpp.ac.in>
2024-11-30 13:18:44 +0530

    Remove unused humidity and wind icons from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,8 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import clear_icon from "./assets/clear.png";
 import cloud_icon from "./assets/cloud.png";
 import drizzle_icon from "./assets/drizzle.png";
-import humidity_icon from "./assets/humidity.png";
 import rain_icon from "./assets/rain.png";
 import snow_icon from "./assets/snow.png";
-import wind_icon from "./assets/wind.png";
 function App() {
   const [weatherData, setWeatherData] = useState(null);
   const [forecastData, setForecastData] = useState([]);
